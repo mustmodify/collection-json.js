7737059a9d29fdfd9f091055d0983fb535ae3de4
Johnathon Wright <jw@mustmodify.com>
2018-12-11 18:13:24 -0600

    add non-functioning tests for extensions, basically a duplicate from previous version.

---
+++ test/extensions.test.js
@@ -0,0 +1,9 @@
+describe('extension tests', () => {
+  describe.skip("[errors](https://github.com/mamund/collection-json/blob/master/extensions/errors.md)", () => it("need tests"));
+  describe.skip("[inline](https://github.com/mamund/collection-json/blob/master/extensions/inline.md)", () => it("need tests"));
+  describe.skip("[model](https://github.com/mamund/collection-json/blob/master/extensions/model.md)", () => it("need tests"));
+  describe.skip("[template-validation](https://github.com/mamund/collection-json/blob/master/extensions/template-validation.md)", () => it("need tests"));
+  describe.skip("[templates](https://github.com/mamund/collection-json/blob/master/extensions/templates.md)", () => it("need tests"));
+  describe.skip("[uri-templates](https://github.com/mamund/collection-json/blob/master/extensions/uri-templates.md)", () => it("need tests"));
+  describe.skip("[value-types](https://github.com/mamund/collection-json/blob/master/extensions/value-types.md)", () => it("need tests"));
+})
